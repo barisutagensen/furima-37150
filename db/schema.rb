0195c206bee458f68099db2e5f8f4c43cf2554f1
barisutagensen <f.kawano08@gmail.com>
2022-02-01 16:34:07 +0900

    itemsモデル、テーブル、コントローラー作成。indexビュー配置。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_061812) do
+ActiveRecord::Schema.define(version: 2022_02_01_071553) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
