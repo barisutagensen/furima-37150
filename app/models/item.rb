eae382220c4e15c05e460ddb5296a52bb864e39a
barisutagensen <f.kawano08@gmail.com>
2022-02-05 10:10:33 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,15 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to       :user
 
-  validates :name,             null: false
-  validates :description,      null: false
-  validates :price,            null: false
-  validates :category_id,      numericality: { other_than: 1, message: "カテゴリーを選択してください"}
-  validates :condition_id,     numericality: { other_than: 1, message: "商品の状態を選択してください"}
-  validates :shipping_fee_id,  numericality: { other_than: 1, message: "配送料の負担を選択してください"}
-  validates :prefectures_id,   numericality: { other_than: 1, message: "発送元の地域を選択してください"}
-  validates :shipping_time_id, numericality: { other_than: 1, message: "発送までの日数を選択してください"}
+  validates :image,             presence: true
+  validates :name,             presence: true
+  validates :description,      presence: true
+  validates :price,            presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :category_id,      numericality: { other_than: 1, message: "を選択してください"}
+  validates :condition_id,     numericality: { other_than: 1, message: "を選択してください"}
+  validates :shipping_fee_id,  numericality: { other_than: 1, message: "を選択してください"}
+  validates :prefectures_id,   numericality: { other_than: 1, message: "を選択してください"}
+  validates :shipping_time_id, numericality: { other_than: 1, message: "を選択してください"}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
