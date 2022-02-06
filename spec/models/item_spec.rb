543e3609737a349c9bcc4a2db9d158014f807730
barisutagensen <f.kawano08@gmail.com>
2022-02-06 16:14:06 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -35,8 +35,20 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Category を選択してください')
       end
 
+      it '商品のカテゴリーが---だと出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category を選択してください')
+      end
+
       it '商品の状態の情報がないと出品できない' do
         @item.condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Condition を選択してください')
+      end
+
+      it '商品の状態が---だと出品できない' do
+        @item.condition_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include('Condition を選択してください')
       end
@@ -47,14 +59,32 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Shipping fee を選択してください')
       end
 
+      it '配送料負担の情報が---だと出品できない' do
+        @item.shipping_fee_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping fee を選択してください')
+      end
+
       it '発送元の地域の情報がないと出品できない' do
         @item.prefectures_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include('Prefectures を選択してください')
       end
 
+      it '発送元の地域の情報が---だと出品できない' do
+        @item.prefectures_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Prefectures を選択してください')
+      end
+
       it '発送までの日数の情報がないと出品できない' do
         @item.shipping_time_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping time を選択してください')
+      end
+
+      it '発送までの日数の情報が---だと出品できない' do
+        @item.shipping_time_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping time を選択してください')
       end
@@ -81,6 +111,12 @@ RSpec.describe Item, type: :model do
         @item.price = '１０００'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+
+      it '価格が半角英数字混合では出品できない' do
+        @item.price = '10ab'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
     end
   end
