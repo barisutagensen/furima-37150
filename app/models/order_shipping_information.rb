3bd4cb3b4dc701330b2890c6824c07a955091fc2
barisutagensen <f.kawano08@gmail.com>
2022-02-13 18:21:46 +0900

    テストコード実装

--- app/models/order_shipping_information.rb
+++ app/models/order_shipping_information.rb
@@ -5,13 +5,13 @@ class OrderShippingInformation
 
   with_options presence: true do
     validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefectures_id, numericality: { other_than: 1, message: 'を選択してください' }
     validates :municipalities
     validates :address
     validates :telephone_number, format: { with: /\A\d{10,11}\z/ }
     validates :user_id
     validates :item_id
   end
-    validates :prefectures_id, numericality: { other_than: 1, message: 'を選択してください' }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
---
+++ spec/factories/order_shipping_informations.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :order_shipping_information do
+    post_code        { '123-4567' }
+    prefectures_id   { 2 }
+    municipalities   { '札幌市' }
+    address          { '123' }
+    telephone_number { '09012345678' }
+  end
+end
