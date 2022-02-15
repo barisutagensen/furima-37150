f6befc937c3dae4e83539a439ce30e23e6a969a5
barisutagensen <f.kawano08@gmail.com>
2022-02-15 17:42:59 +0900

    orderコントローラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,14 +22,14 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_shipping_information).permit(:post_code, :prefectures_id, :municipalities, :address, :building_name, :telephone_number).merge(
-      user_id: current_user.id, item_id: @item.id, token: params[:token], price: @item.price
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
     )
   end
 
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: order_params[:price],
+      amount: @item.price,
       card: order_params[:token],
       currency: 'jpy'
     )
